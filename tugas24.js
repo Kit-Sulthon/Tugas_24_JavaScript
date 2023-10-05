92ed36f2db41105c82fbc58f70fc2257401f69a1
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 16:28:41 +0700

    Tugas 24 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,13 @@
+function Tugas24() {
+  const soal1 = ["32", "33", "16", "40", "9", "7", "11", "2", "44", "1", "66"];
+  const soal2 = ["32", "33", "16", "40", "9", "7", "11", "2", "44", "1", "66"];
+  const soal3 = ["32", "33", "16", "40", "9", "7", "11", "2", "44", "1", "66"];
+  soal1.sort();
+  soal3.sort().reverse();
+
+  console.log("Sebelumnya :", soal2);
+  console.log("Ascending :", soal1);
+  console.log("Descending :", soal3);
+}
+
+console.log(Tugas24());
